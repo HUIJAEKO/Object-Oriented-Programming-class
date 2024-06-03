6a6f5367ca527e53f025db6d579f15e3a6de6efd
HUIJAEKO <xrhgmlwox@naver.com>
2024-06-04 02:08:34 +0900

    exam

---
+++ src/ExamPractice/Geometric.java
@@ -0,0 +1,11 @@
+package ExamPractice;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Geometric {
+    String[] array = {"123", "111", "121"};
+    ArrayList<String> arr = new ArrayList<>(Arrays.asList(array));
+    String[] array1 = new String[arr.size()];
+
+}
