c058e9d4664c627a970e5b4c5468277e583d9b3b
HUIJAEKO <xrhgmlwox@naver.com>
2024-06-07 13:55:32 +0900

    final

--- src/ExamPractice/Geometric.java
+++ src/ExamPractice/Geometric.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Geometric {
-    String[] array = {"123", "111", "121"};
-    ArrayList<String> arr = new ArrayList<>(Arrays.asList(array));
-    String[] array1 = new String[arr.size()];
+
 
 }
